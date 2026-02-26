f33d95fbeb94c07271d275fd8396f4f073df9090
conalec25 <facturacion@conalec.com>
2026-02-25 20:23:21 -0500

    Add ListaFacturas component for displaying SharePoint lists

---
+++ src/components/ListaFacturas.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListaFacturas = () => {
+    return (
+        <div>
+            <h1>Lista de Facturas</h1>
+            {/* Aquí puedes implementar la lógica para mostrar las listas de SharePoint */}
+        </div>
+    );
+};
+
+export default ListaFacturas;
